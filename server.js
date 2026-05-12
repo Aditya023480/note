83e4cc8f811557a60e6e6938458fa44cb21e2ec7
unknown <adityachauhan200079@gmail.com>
2026-05-13 01:08:43 +0530

    router setup

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import route from "./routes.js";
+
+
 
 const app = express();
 
+app.use(express.json());
+app.use(route)
+
 app.listen(8080 , ()=>{
-    console.log("Server Started!")
+    console.log("Server Started!");
 })
