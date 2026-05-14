776d260cb1b1b2c228285d2319b0057302bf993c
unknown <adityachauhan200079@gmail.com>
2026-05-14 23:21:10 +0530

    adding /createnote route

--- routes.js
+++ routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import pool from "./db.js";
+
 
 const route = Router();
 
 route.get("/" , (req,res)=>{
     res.send("OK!");
 })
+
+route.post("/note" ,async (req,res)=>{
+
+    let {title , body} = req.body;
+
+    const create_note = await pool.query("INSERT INTO notes(title , body) VALUES ($1 , $2)" , [title , body]);
+
+    
+
+})
+
+
 
 export default route;
